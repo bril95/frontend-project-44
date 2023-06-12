0c7c9dbe9d0a1c9385a75ee7bd8a9cfd5d0304f3
bril95 <bril.vadim@mail.ru>
2023-06-12 17:01:53 +0300

    work file brain-progression.js

---
+++ games/progression.js
@@ -0,0 +1,27 @@
+import mainPartOfApp from '../src/index.js';
+
+const progression = () => {
+  //  спрашивает имя
+  const howToPlay = 'What number is missing in the progression?';
+  // цикл
+  const gamePart = () => {
+    const startProgression = Math.floor(Math.random() * 10);
+    const arr = [];
+    arr.push(startProgression);
+    const stepProgression = Math.floor(Math.random() * 10) + 1;
+    let nextElement = startProgression + stepProgression;
+    arr.push(nextElement);
+    for (let i = 0; i < 8; i += 1) {
+      nextElement += stepProgression;
+      arr.push(nextElement);
+    }
+    const randomElement = Math.floor(Math.random() * 10);
+    const rightAnswer = arr[randomElement];
+    arr[randomElement] = '..';
+    const question = `Question: ${arr}`;
+    return [question, rightAnswer];
+  };
+  mainPartOfApp(howToPlay, gamePart);
+};
+
+export default progression;
