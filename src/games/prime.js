32ebd344f2c1c1d455b3272154416c94ee380871
bril95 <bril.vadim@mail.ru>
2023-06-18 20:02:02 +0300

    fix brain-prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,21 +4,21 @@ import random from '../utils.js';
 const gameRule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function isPrime(num) {
-  for (let i = 2; i < num; i += 1) {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i < Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
   return true;
 }
 
-const prime = () => {
-  const gamePart = () => {
-    const randomNumber = random(2, 100);
-    const question = `${randomNumber}`;
-    let rightAnswer = isPrime(randomNumber);
-    rightAnswer = rightAnswer ? 'yes' : 'no';
-    return [question, rightAnswer];
-  };
-  startGame(gameRule, gamePart);
+const createRound = () => {
+  const randomNumber = random(0, 100);
+  const question = `${randomNumber}`;
+  let rightAnswer = isPrime(randomNumber);
+  rightAnswer = rightAnswer ? 'yes' : 'no';
+  return [question, rightAnswer];
 };
 
-export default prime;
+export default () => startGame(gameRule, createRound);
